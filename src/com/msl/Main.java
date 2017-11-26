aac464d2b373367b66a34aca3a5c1eb840ffc78a
Anıl Doğru <anil.dogru@ozu.edu.tr>
2017-11-27 01:39:09 +0300

    Created

--- src/com/msl/Main.java
+++ src/com/msl/Main.java
@@ -1,8 +1,11 @@
 package com.msl;
 
+import java.sql.Connection;
+
 public class Main {
+    private Connection conn;
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
