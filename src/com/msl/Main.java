f3b696324ac56f6fec214e7797895ec6e2ed793e
Anıl Doğru <anil.dogru@ozu.edu.tr>
2017-11-27 02:00:08 +0300

    Connected to database.

--- src/com/msl/Main.java
+++ src/com/msl/Main.java
@@ -1,11 +1,39 @@
 package com.msl;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class Main {
-    private Connection conn;
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306/msl";
+    private static final String CONNECTION_USER = "root";
+    private static final String CONNECTION_PASSWORD = "Msl123456";
 
     public static void main(String[] args) {
+        System.out.println("Connecting...");
+        Connection conn = Connect();
+        if (conn == null) {
+            System.err.println("Connection failed.");
+            return;
+        }
 
+        System.out.println("Connected.");
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM User");
+            while (rs.next()) {
+                System.out.println("User: " + rs.getString("UserName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Connection Connect() {
+        try {
+            return DriverManager.getConnection(CONNECTION_URL, CONNECTION_USER, CONNECTION_PASSWORD);
+        } catch (SQLException e) {
+            throw new IllegalStateException("Error!", e);
+        }
     }
 }
